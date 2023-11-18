db00476a1dd1a13041a4a42b24be103aa45374d4
Ross <rossjameson1623@gmail.com>
2023-11-18 08:43:12 -0600

    update pokemon card styles

--- src/components/pokemanCard.svelte
+++ src/components/pokemanCard.svelte
@@ -1,11 +1,50 @@
 <script>
 	export let pokemancard;
+
+	let isHovered = false;
+
+	function handleHover() {
+		isHovered = !isHovered;
+	}
 </script>
 
 <a
-	class="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-md hover:shadow-lg flex flex-col items-center"
+	on:mouseenter={handleHover}
+	on:mouseleave={handleHover}
+	class=" bg-gray-100  pokemonCard"
 	href={`/pokemon/${pokemancard.id}`}
 >
 	<img class="h-40 w-40" src={pokemancard.image} alt={pokemancard.name} />
-	<h2 class="uppercase text-2xl">{pokemancard.id}. {pokemancard.name}</h2>
+	{#if isHovered}
+		<h2 class="uppercase text-2xl name">{pokemancard.name}</h2>
+	{/if}
 </a>
+
+<style>
+	.pokemonCard {
+		padding: 6px;
+		color: black;
+		text-align: center;
+		border-radius: 8px;
+		box-shadow: 0 0 5px rgba(0, 0, 0, 0.1), 0 0 10px rgba(0, 0, 0, 0.2);
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: fit-content;
+		min-width: 200px;
+		margin: 5px;
+		position: relative;
+	}
+	.pokemonCard:hover {
+		box-shadow: 0 0 10px rgba(0, 123, 255, 0.7);
+	}
+	.name {
+		position: absolute;
+		bottom: 0;
+		width: 100%;
+	}
+	a {
+		text-decoration: none;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,20 +19,32 @@
 	}
 </script>
 
-<h1 class="text-4xl text-center my-8 uppercase">svelte pokedex</h1>
-<input
-	class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
-	type="text"
-	placeholder="search pokemon"
-	bind:value={searchTerm}
-/>
-<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-	{#each filteredPokemon as pokeman}
-
-		<PokemanCard pokemancard={pokeman} />
-
-	{/each}
+<div class="mainPageWrap">
+	<h1 class="text-4xl text-center my-8 uppercase">svelte pokedex</h1>
+	<input
+		class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+		type="text"
+		placeholder="search pokemon"
+		bind:value={searchTerm}
+	/>
+	<div class="pokemonCardWrap">
+		{#each filteredPokemon as pokeman}
+			<PokemanCard pokemancard={pokeman} />
+		{/each}
+	</div>
 </div>
 
 <style>
+	.mainPageWrap {
+		padding: 0px 16px;
+	}
+	/* py-4 grid gap-4 md:grid-cols-2 grid-cols- */
+
+	.pokemonCardWrap {
+		padding: 0px 4px;
+		display: flex;
+		justify-content: center;
+		flex-wrap: wrap;
+		width: 100dvw;
+	}
 </style>
