aabb26a4976879221c52e21ea9b978a2c84f0120
Ross <rossjameson1623@gmail.com>
2023-11-18 09:10:09 -0600

    clean up pokemon cards and input placements

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,6 @@
 	import { pokemon } from '../stores/pokestore';
 	import PokemanCard from '../components/pokemanCard.svelte';
 
-	// console.log('pokemon = ', $pokemon);
-
 	let searchTerm = '';
 	let filteredPokemon = [];
 
@@ -21,30 +19,49 @@
 
 <div class="mainPageWrap">
 	<h1 class="text-4xl text-center my-8 uppercase">svelte pokedex</h1>
-	<input
-		class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
-		type="text"
-		placeholder="search pokemon"
-		bind:value={searchTerm}
-	/>
-	<div class="pokemonCardWrap">
-		{#each filteredPokemon as pokeman}
-			<PokemanCard pokemancard={pokeman} />
-		{/each}
+	<div class="piWrap">
+		<input class="pokeInput" type="text" placeholder="search pokemon" bind:value={searchTerm} />
+	</div>
+	<div class="doubleWrap">
+		<div class="pokemonCardWrap">
+			{#each filteredPokemon as pokeman}
+				<PokemanCard pokemancard={pokeman} />
+			{/each}
+		</div>
 	</div>
 </div>
 
 <style>
 	.mainPageWrap {
 		padding: 0px 16px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
 	}
-	/* py-4 grid gap-4 md:grid-cols-2 grid-cols- */
+	.doubleWrap {
+		display: flex;
+		width: 100%;
+		justify-content: center;
+	}
 
+	.pokeInput {
+		width: 50dvw;
+		border-radius: 8px;
+		font-size: 16px;
+		padding: 0px 4px;
+		border: 2px solid lightcoral;
+	}
+	.piWrap {
+		display: flex;
+		justify-content: center;
+		height: 50px;
+	}
 	.pokemonCardWrap {
 		padding: 0px 4px;
 		display: flex;
 		justify-content: center;
 		flex-wrap: wrap;
 		width: 100dvw;
+		max-width: 1000px;
 	}
 </style>
