ccce8451512ae4b601c5e41ba71b0e8750d4252b
Ross <rossjameson1623@gmail.com>
2024-04-20 07:55:22 -0500

    componatized the game input

---
+++ src/components/gameInput/gameInput.svelte
@@ -0,0 +1,53 @@
+<script>
+	export let name = '';
+	export let correct_Answer = 'Rosco';
+	export let guess = 0;
+	export let correct = false;
+
+	$: if (name === correct_Answer) {
+		correct = true;
+	}
+
+	function submit_Answer() {
+		if (name != correct_Answer) {
+			correct = false;
+			guess++;
+			if (guess >= 3) {
+				window.location.href = '/locations';
+			}
+		}
+	}
+</script>
+
+<div class="form__group field  submission-wrap">
+	<input
+		autocomplete="off"
+		bind:value={name}
+		type="input"
+		class="form__field text-black"
+		placeholder="Name"
+		name="name"
+		id="name"
+		required
+	/>
+	<label for="name" class="form__label" />
+	<button class="submit-answers" on:click={submit_Answer}>submit</button>
+</div>
+
+<style>
+	.submission-wrap {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		border-radius: 8px;
+		padding: 10px;
+		border: solid black 1px;
+		backdrop-filter: blur(10px);
+		-webkit-backdrop-filter: blur(10px);
+		box-shadow: inset 25px 25px 50px #758f78, inset -25px -25px 50px #9fc1a2;
+	}
+
+	.submission-wrap input {
+		text-align: center;
+	}
+</style>
--- src/routes/entergame/[id].svelte
+++ src/routes/entergame/[id].svelte
@@ -1,47 +1,32 @@
-<script>
+<script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
-	import About from '../howto/character.svelte';
 	import { pokemon } from '../../stores/pokestore.js';
 	import { openaiResponse } from '../../stores/aistore.js';
-	import PokemanCard from '../../components/pokemanCard.svelte';
 	import wizard from './openSourcerer.jpg';
+	import GameInput from '../../components/gameInput/gameInput.svelte';
+	import PokemanCard from '../../components/pokemanCard.svelte';
 	import { load } from '../pokemon/[id].svelte';
 
 	let name = '';
-	let searchTerm = '';
 	let correct_Answer = 'Rosco';
 	let correct = false;
-	let filteredPokemon = [];
 	let random = randomInteger(1, 898);
-	let area;
 	let loaded = false;
-
-	let number;
 	let startingMessage = $openaiResponse.choices[0].message.content;
-
 	let guess = 0;
-	let options = '';
 	let adventureOptions = '';
 	let title = '';
 	let descriptionParts = '';
 	let description = '';
 	let introduction = '';
+	let storySelected = '';
+	let fighterOptions = ['fight', 'examine', 'catch', 'run'];
+	let selectedStory = '';
+	let showSubmit = false;
 
-	$: if (name === correct_Answer) {
-		correct = true;
-	}
-
-	const typingDelay = 800;
-
-	function submit_Answer() {
-		if (name != correct_Answer) {
-			correct = false;
-			guess++;
-			if (guess >= 3) {
-				window.location.href = '/locations';
-			}
-		}
-	}
+	//TODO : Inventory, Weapon
+	//select story start, send that to open api and get a new response
+	// allow typing into Input to ask DM questions
 
 	let reset = (number) => {
 		if (number >= 100) {
@@ -52,6 +37,15 @@
 
 	function randomInteger(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	function submitStartingStory(story, title) {
+		storySelected = story;
+		showSubmit = !showSubmit;
+
+		selectedStory = title;
+		console.log('selcted st = ', selectedStory);
+		console.log('story selected = ', storySelected);
 	}
 
 	onMount(() => {
@@ -76,81 +70,78 @@
 </script>
 
 <div class="background">
-	About
-
-	<h1>Enter the Endless Adventure</h1>
+	{#if !correct}
+		<h1 class="enterTitle">Enter the Endless Adventure</h1>
+		<div>But First, Submit My Name</div>
+	{/if}
 
 	<div class="content">
-		<span>
-			<div class="form__group field m-12">
-				<input
-					autocomplete="off"
-					bind:value={name}
-					type="input"
-					class="form__field text-black"
-					placeholder="Name"
-					name="name"
-					id="name"
-					required
-				/>
-				<label for="name" class="form__label" />
-				<button on:click={submit_Answer}>submit</button>
-			</div>
+		{#if !correct}
+			<GameInput bind:correct_Answer bind:name bind:guess bind:correct />
+
 			<div class="myName">
 				{name}
 			</div>
-			{#if correct}
-				<!-- <div class="blurb">
-					Good Guess, oh shit! you have encountered a wild beast, what ever will you do?!
-				</div> -->
-				<p class="fightTime">time to fight</p>
-				<div class="fightArena">
-					{#each $pokemon as pokeman}
-						{#if pokeman.id === random}
-							<div>
-								<img src={wizard} alt="" width="100" height="100" />
-								<span class="flex flex-col">
-									<button class="bg-slate-500 m-2 h-4 w-24">fight</button>
-									<button class="bg-slate-500 m-2 h-4 w-24">examine</button>
-									<button class="bg-slate-500 m-2 h-4 w-24">catch</button>
-									<button class="bg-slate-500 m-2 h-4 w-24">run</button>
-								</span>
-							</div>
-							<div class="monster">
-								<img src={pokeman.image} alt="" width="100" height="100" />
-							</div>
-							<div class="shadow" style="" />
-						{/if}
-					{/each}
-				</div>
-			{/if}
-			<!-- {#if guess >= 1 && !correct} -->
+		{/if}
+
+		{#if correct}
 			<div class="container">
 				{#if loaded}
 					<button class="option-button">
 						{introduction}
 					</button>
 					{#each adventureOptions as { title, description }}
-						<button class="option-button">
-							<!-- {introduction} -->
+						<button on:click={submitStartingStory(description, title)} class="btn">
 							<h2 class="title">{title}</h2>
 							<div class="description">{description}</div>
 						</button>
+						{#if selectedStory === title && showSubmit}
+							<div class="myName">
+								{name}
+							</div>
+
+							<GameInput bind:correct_Answer bind:name bind:guess bind:correct />
+						{/if}
 					{/each}
 				{/if}
 			</div>
-			{#if guess >= 1 && !correct}
-				<div class="text-red-50">
-					X
-					{#if guess >= 2}
-						X
-						{#if guess >= 3}
-							X
-						{/if}
+
+			<p class="fightTime">time to fight</p>
+			<div class="fightArena">
+				{#each $pokemon as pokeman}
+					{#if pokeman.id === random}
+						<div class="creatures">
+							<div class="character-wrap">
+								<img src={wizard} alt="" width="100" height="100" />
+							</div>
+
+							<div class="monster">
+								<img src={pokeman.image} alt="" width="100" height="100" />
+							</div>
+						</div>
+
+						<div class="flex  character-controls">
+							{#each fighterOptions as option}
+								<button class="btn fight-options">
+									{option}
+								</button>
+							{/each}
+						</div>
 					{/if}
-				</div>
-			{/if}
-		</span>
+				{/each}
+			</div>
+		{/if}
+		{#if guess >= 1 && !correct}
+			<div class="text-red-50">
+				X
+				{#if guess >= 2}
+					X
+					{#if guess >= 3}
+						X
+					{/if}
+				{/if}
+			</div>
+		{/if}
 
 		{#if guess >= 1 && !correct}
 			<div>thats not my name bro</div>
@@ -161,16 +152,30 @@
 <style>
 	@import '../../../public/fonts.css';
 
-	.blurb {
-		max-width: 500px;
+	.container {
+		display: flex;
+		flex-direction: column;
+		font-family: 'MapleMono-Italic', sans-serif;
+		justify-content: center;
 	}
+
+	.enterTitle {
+		margin: 16px 0px;
+	}
+
 	.title {
 		display: flex;
 		width: 100%;
 		justify-content: center;
 		text-align: center;
-		font-family: 'MapleMono', sans-serif;
+		font-family: 'MapleMono-Italic', sans-serif;
+		/* border: linear-gradient(to bottom left, #e65054, #f0c33c 70%) 1px solid; */
+		color: white;
+		/* color: #e65054; */
+		/* #f0c33c */
+		/* background: linear-gradient(to bottom left, #e65054, #f0c33c 70%); */
 	}
+
 	.option-button {
 		display: flex;
 		flex-direction: column;
@@ -186,11 +191,49 @@
 		cursor: pointer;
 		outline: none;
 		font-family: 'MapleMono-Italic', sans-serif;
-		max-width: 600px;
 	}
 
-	.option-button h2 {
-		margin: 0;
+	.character-controls {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+	.character-wrap {
+		height: fit-content;
+	}
+
+	.monster {
+		height: fit-content;
+	}
+	.creatures {
+		display: flex;
+		width: 100%;
+		justify-content: space-evenly;
+	}
+
+	.fight-options {
+		font-family: 'MapleMono-Italic', sans-serif;
+	}
+	.btn {
+		margin: 10px;
+		padding: 10px;
+		font-size: 1.1em;
+		cursor: pointer;
+		border: none;
+		border-radius: 5px;
+		transition: all 0.4s;
+	}
+
+	.btn:hover {
+		border-radius: 5px;
+		transform: translateY(-10px);
+		box-shadow: 0 7px 0 -2px #f85959, 0 15px 0 -4px #39a2db, 0 16px 10px -3px #39a2db;
+	}
+
+	.btn:active {
+		transition: all 0.2s;
+		transform: translateY(-5px);
+		box-shadow: 0 2px 0 -2px #f85959, 0 8px 0 -4px #39a2db, 0 12px 10px -3px #39a2db;
 	}
 
 	.option-button:hover {
@@ -204,28 +247,33 @@
 		}
 	}
 	.fightArena {
+		width: 100%;
 		display: flex;
 		justify-content: space-evenly;
+		flex-direction: column;
+		margin-bottom: 40px;
 	}
 	.myName {
 		display: flex;
 		justify-content: center;
+		/* Set the width of the container */
+		overflow-wrap: break-word; /* Ensures words break to prevent overflow */
+		word-wrap: break-word; /* Older browsers support, same as overflow-wrap */
+		white-space: normal; /* Ensures whitespace is handled normally, allowing wrapping */
 	}
-	.shadow {
-		background-image: black;
-		padding: 10px;
-		height: 5px;
-		width: 10px;
-		border-radius: 50%;
-	}
+
 	.fightTime {
+		padding: 20px 0px;
 		display: flex;
 		align-items: center;
 		justify-content: center;
+		font-family: 'MapleMono-Italic', sans-serif;
 	}
+
 	h1 {
 		color: white;
 	}
+
 	.background {
 		background-color: black;
 		background-image: radial-gradient(rgba(0, 150, 0, 0.75), black 120%);
@@ -243,9 +291,11 @@
 		justify-content: space-evenly;
 		padding: 16px 32px;
 	}
+
 	:global(*) {
 		box-sizing: border-box;
 	}
+
 	.background {
 		background-image: radial-gradient(rgba(0, 150, 0, 0.75), black 120%);
 		display: flex;
@@ -254,37 +304,5 @@
 		justify-content: center;
 		align-items: center;
 		touch-action: none;
-	}
-
-	.box {
-		background: radial-gradient(rgba(0, 150, 0, 0.75), black 420%);
-		border-radius: 30px;
-
-		position: absolute;
-		top: calc(50% - 150px / 2);
-		left: calc(50% - 150px / 2);
-	}
-	.drag-area {
-		opacity: 0.2;
-
-		position: absolute;
-		width: 400px;
-		height: 400px;
-		border-radius: 30px;
-		top: calc(50% - 150px);
-		left: calc(50% - 150px);
-	}
-	.unselectable {
-		-webkit-touch-callout: none;
-		-webkit-user-select: none;
-		-khtml-user-select: none;
-		-moz-user-select: none;
-		-ms-user-select: none;
-		user-select: none;
-	}
-	.center {
-		justify-content: center;
-		align-items: center;
-		display: flex;
 	}
 </style>
